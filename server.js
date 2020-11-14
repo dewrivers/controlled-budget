ed2e147fd2b42c9f7d4d78c99040fd5a487ef8ff
Rocio Rios <rrocio46@yahoo.es>
2020-11-13 23:38:39 -0500

    fixing a bug to deploy in heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 
