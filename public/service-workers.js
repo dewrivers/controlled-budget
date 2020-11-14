ff0a7f646eff909a79045433824e72efc83367b5
Rocio Rios <rrocio46@yahoo.es>
2020-11-13 21:25:39 -0500

    adding code to service-workers

--- public/service-workers.js
+++ public/service-workers.js
@@ -12,3 +12,58 @@ let urlsCache = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
 ];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            console.log('Successfully Cache!!');
+            return cache.addAll(urlsCache);
+        })
+    )
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                if(key !== DATA_CACHE_NAME && key !== CACHE_NAME)
+                    {
+                        console.log('Old cache removed', key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    )
+    self.clients.claim();
+});
+
+// 
+
+self.addEventListener('fetch', (event) => {
+    if(event.request.url.includes('/api/')){
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache =>
+            {
+            return fetch(event.request)
+            .then(response => {
+                if(response.status === 200){
+                    cache.put(event.request.url, response.clone());
+                }
+                return response;
+            })
+            .catch(error => {
+             return cache.match(event.request);
+            });
+
+        }).catch(error => console.log(error))
+        );
+    // );
+    return;
+}
+event.respondWith(
+
+)
+});
+
