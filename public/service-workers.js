b0960a9e0f20a223b3288acb13aede2427915a64
Rocio Rios <rrocio46@yahoo.es>
2020-11-13 20:02:31 -0500

    adding manifest

---
+++ public/service-workers.js
@@ -0,0 +1,14 @@
+let CACHE_NAME = 'static-cache-v2';
+let DATA_CACHE_NAME = 'data-cache-v1';
+
+let urlsCache = [
+    '/',
+    '/db.js',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/service-workers.js',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
